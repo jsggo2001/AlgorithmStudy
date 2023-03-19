aebfdd50d3479f53ff3129c8e314b21ad8ccb2e2
정승구 <jsggo2001@naver.com>
2023-03-19 16:54:16 +0900

    first

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -6,6 +6,6 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("asd");
     }
 }
